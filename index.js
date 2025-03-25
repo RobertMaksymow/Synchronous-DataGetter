aad71b5c6f9c280da9c88f639c86a6f0b96bec22
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-03-25 22:18:45 +0000

    Added express, taapi, nodemon and env. Server works, makes initial call and get result from taapi.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+import express from "express";
+import dotenv from "dotenv";
+import Taapi from "taapi";
+
+dotenv.config();
+const PORT = process.env.port || 3000;
+
+const app = express();
+const taapi = new Taapi.default(process.env.TAAPI_KEY);
+
+taapi.setDefaultExchange("binance");
+
+taapi.getIndicator("rsi", "BTC/USDT", "1h").then((rsi) => {
+  console.log(rsi);
+});
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port: ${PORT}`);
+});
