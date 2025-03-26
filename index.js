ccc7b075c8016deb459904ddfdad0e8bb8db76db
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-03-26 13:13:44 +0000

    It fetch bulk data correctly from API using Axios.

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
+import axios from "axios";
 import Taapi from "taapi";
 
 dotenv.config();
 const PORT = process.env.port || 3000;
 
 const app = express();
-const taapi = new Taapi.default(process.env.TAAPI_KEY);
 
-taapi.setDefaultExchange("binance");
-
-// taapi.getIndicator("rsi", "BTC/USDT", "1h").then((rsi) => {
-//   console.log(rsi);
-//   console.log(typeof rsi);
-// });
-
-// taapi.getExchangeSymbols("crypto", "binance", "USDT").then((symbols) => {
-//   console.log(symbols);
-// });
-
-// Reset Bulk Query
-taapi.resetBulkConstructs();
-
-// Add calculations
-taapi.addCalculation("rsi", "BTC/USDT", "1h", "rsi_1h");
-// taapi.addCalculation("macd", "BTC/USDT", "1h", "macd_1h");
-// taapi.addCalculation("ema", "BTC/USDT", "1h", "ema_fast_1h", {
-//   period: 9,
-//   backtrack: 1,
-// });
-// taapi.addCalculation("ema", "BTC/USDT", "1h", "ema_slow_1h", {
-//   period: 20,
-//   backtrack: 1,
-// });
-
-// Execute Crypto request
-taapi
-  .executeBulk("crypto")
-  .then((results) => {
-    console.log(results);
+await axios
+  .post("https://api.taapi.io/bulk", {
+    secret: `${process.env.TAAPI_KEY}`,
+    construct: {
+      exchange: "binance",
+      symbol: "BTC/USDT",
+      interval: "1h",
+      indicators: [
+        {
+          // Relative Strength Index
+          indicator: "rsi",
+        },
+        {
+          // Chaikin Money Flow
+          indicator: "cmf",
+          period: 20, // Override the default 14
+        },
+        {
+          // MACD Backtracked 1
+          id: "my_custom_id",
+          indicator: "macd",
+          backtrack: 1,
+        },
+      ],
+    },
+  })
+  .then((response) => {
+    console.log(response.data.data[0]);
+    console.log(response.data.data[1]);
+    console.log(response.data.data[2]);
   })
   .catch((error) => {
     console.error(error);
