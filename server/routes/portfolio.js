b8984910fa6bcba8027bc77eb2bd29af3cc0c6d8
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-05-29 15:43:09 +0100

    Controllers for GET ALL, GET BY ID, CREATE done and works.

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,39 +1,18 @@
 import express from "express";
-import Portfolio from "../modelsDB/Portfolio_model.js";
+
+import {
+  getPortfolioTrades,
+  getPortfolioTradeById,
+  createPortfolioTrade,
+} from "../controllers/portfolioController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "GET all Portfolio coins",
-    status: "success",
-  });
-});
+router.get("/", getPortfolioTrades);
 
-router.get("/:id", (req, res) => {
-  res.json({
-    message: `GET Portfolio coin with ID: ${req.params.id}`,
-    status: "success",
-  });
-});
+router.get("/:id", getPortfolioTradeById);
 
-router.post("/", async (req, res) => {
-  const { transactionType, price, quantity, totalValue, coin, priceInBTC } =
-    req.body;
-  try {
-    const portfolioTrade = await Portfolio.create({
-      transactionType,
-      price,
-      quantity,
-      totalValue,
-      coin,
-      priceInBTC,
-    });
-    res.status(200).json(portfolioTrade);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createPortfolioTrade);
 
 router.delete("/:id", (req, res) => {
   res.json({
